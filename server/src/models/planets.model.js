80321e93f66d77055cbfc0a7ccba8acce75ef3de
its-kaushik <mkaushik0033@gmail.com>
2023-02-23 18:02:04 +0530

    feeding actual data to getallplanets route

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,43 @@
-const planets = [] ;
+const {parse} = require('csv-parse');
+const fs = require('fs')
+const path = require('path')
 
-module.exports = planets ;
+const habitablePlanets = []
+
+function isHabitable (planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
+
+function loadPlanetsData () {
+
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+            .pipe(parse({
+                comment: '#',
+                columns: true,
+            }))
+
+            .on('data', (planet) => {
+                if(isHabitable(planet))
+                    habitablePlanets.push(planet)
+            })
+
+            .on('error', (err) => {
+                console.log(`Error while reading planet data stream : ${err}`)
+                reject(err)
+            })
+
+            .on('end', () => {
+                console.log(`${habitablePlanets.length} planets found !!`)
+                resolve()
+            })
+    })
+    
+}
+
+module.exports = {
+    planets: habitablePlanets,
+    loadPlanetsData
+}
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,18 @@
 const http = require('http');
 const app = require('./app');
+const { loadPlanetsData } = require('./models/planets.model');
 
 const server = http.createServer(app) ;
 const PORT = process.env.PORT || 4000 ;
 
-server.listen( PORT, () => {
-    console.log(`The server is up and running on Port : ${PORT}`) ;
-}) ;
+async function createServer () {
 
+    await loadPlanetsData()
+
+    server.listen( PORT, () => {
+        console.log(`The server is up and running on Port : ${PORT}`) ;
+    }) ;    
+
+}
+
+createServer()
